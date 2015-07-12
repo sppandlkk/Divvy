188ee43f31dfa467e76e90c40ed04ec6a193c626
sppandlkk <u900222@gmail.com>
2015-07-12 14:37:41 -0500

    fix bugs, adding heatmap, adding hist

--- server.R
+++ server.R
@@ -1,9 +1,10 @@
 library(shiny)
 library(ggplot2)
 library(ggmap)
+library(gplots)
 
 # read in the data
-trips <- read.csv("./data/Divvy_Trips_2013_t.csv")
+trips <- read.csv("./data/Divvy_Trips_2013.csv")
 stations <- read.csv("./data/Divvy_Stations_2013.csv")
 
 #### first restrict to member
@@ -26,6 +27,13 @@ trips_subs$age_cat[trips_subs$age >= 51] <- "51+"
 
 #### cut off old people
 trips_subs <- trips_subs[trips_subs$age <= 90,]
+
+
+#### create month
+#trips_subs$start_month <- format(as.Date(substr(trips_subs$starttime,1,7),"%m/%d/%y"),"%m")
+trips_subs$from_month <- as.numeric(sub("/","",substr(trips_subs$starttime,1,2)))
+trips_subs$from_hour  <- as.numeric(sub(":","",substr(trips_subs$starttime,10,11)))
+trips_subs$to_hour    <- as.numeric(sub(":","",substr(trips_subs$stoptime,10,11)))
 
 function(input, output) {
 		
@@ -54,15 +62,52 @@ function(input, output) {
    	if(input$to_station_name != "ALL"){
      		dd <- dd[dd$to_station_name %in% input$to_station_name,]
      	}
-	tt <- table(dd$from_station_id)
-	merge(data.frame(id=names(tt),cnt=c(tt)),stations[,c("longitude","latitude","id")],by="id")
+	tt1 <- table(dd$from_station_id)
+	merge(data.frame(id=names(tt1),cnt=c(tt1)),stations[,c("longitude","latitude","id")],by="id")
   })
 
 
 #### first panel
 #### for creating the plot
-  output$plot <- renderPlot({
-    p <- qplot(tripduration,data=dataset(),  geom="histogram", binwidth=input$binwidth)
+  output$hist_for_duration <- renderPlot({
+    p <- qplot(tripduration,data=dataset(),  geom="histogram", binwidth=input$binwidth_for_duration)
+
+	facet_col <- "."	
+	facet_row <- "."
+
+    if (input$by_age)	facet_col <- "age_cat"
+    if (input$by_gender)	facet_row <- "gender"
+    
+  
+    facets <- paste(facet_row, '~', facet_col)
+    if (facets != '. ~ .')
+      p <- p + facet_grid(facets)
+
+
+    print(p)
+
+  }, height=600)
+
+  output$hist_for_month <- renderPlot({
+    p <- qplot(from_month,data=dataset(),  geom="histogram", binwidth=1)
+
+	facet_col <- "."	
+	facet_row <- "."
+
+    if (input$by_age)	facet_col <- "age_cat"
+    if (input$by_gender)	facet_row <- "gender"
+    
+  
+    facets <- paste(facet_row, '~', facet_col)
+    if (facets != '. ~ .')
+      p <- p + facet_grid(facets)
+
+
+    print(p)
+
+  }, height=600)
+  output$hist_for_from_hour <- renderPlot({
+    p <- qplot(from_hour,data=dataset(),  geom="histogram", binwidth=1)
 
 	facet_col <- "."	
 	facet_row <- "."
@@ -88,6 +133,12 @@ function(input, output) {
   output$text_to    <- renderText({
 	 paste("You have selected end station :", input$to_station_name)
   })
+  output$text_start1 <- renderText({
+	 paste("You have selected start station :", input$from_station_name)
+  })
+  output$text_to1    <- renderText({
+	 paste("You have selected end station :", input$to_station_name)
+  })
 
 #### plot 
   output$plot_start <- renderPlot({
@@ -100,18 +151,50 @@ function(input, output) {
   output$given_start <- renderText({
 	 paste("Given the start station :", input$from_station_name)
   })
+  output$given_end <- renderText({
+	 paste("Given the end station :", input$to_station_name)
+  })
 #### ggmap
 	output$ggmap_by_start <- renderPlot({
-		gg <- get_map(location = c(lon=-87.65,lat=41.88), zoom = 12)
+		data_temp <- dataset_by_start()
+		lon_default <- -87.65
+		lat_default <-  41.88
+		if(input$from_station_name != "ALL"){
+			lon_default <- stations$longitude[stations$name == input$from_station_name]
+			lat_default <- stations$latitude[stations$name == input$from_station_name]
+		}
+		gg <- get_map(location = c(lon=lon_default,lat=lat_default), zoom = 12)
 		gg <- ggmap(gg,extent="panel")
-		gg <- gg + geom_point(aes(x = longitude, y = latitude, size = sqrt(cnt)), data = dataset_by_start())
+		gg <- gg + geom_point(aes(x = longitude, y = latitude, size = cnt), data = data_temp)
 		### adding the starting
-		gg <- gg + geom_point(aes(x = longitude, y = latitude), size = input$size_to, color= "red", data = stations[stations$name==input$from_station_name,])
+		gg <- gg + geom_point(aes(x = longitude, y = latitude), size = input$size_to, pch=17 , color= "red", data = stations[stations$name==input$from_station_name,])
+		print(gg)
+	},height=600)
+####  ggmpa by end
+	output$ggmap_by_end <- renderPlot({
+		data_temp1 <- dataset_by_to()
+		lon_default <- -87.65
+		lat_default <-  41.88
+		if(input$to_station_name != "ALL"){
+			lon_default <- stations$longitude[stations$name == input$to_station_name]
+			lat_default <- stations$latitude[stations$name == input$to_station_name]
+		}
+		gg <- get_map(location = c(lon=lon_default,lat=lat_default), zoom = 12)
+		gg <- ggmap(gg,extent="panel")
+		gg <- gg + geom_point(aes(x = longitude, y = latitude, size = cnt), data = data_temp1)
+		### adding the starting
+		gg <- gg + geom_point(aes(x = longitude, y = latitude), size = input$size_to, pch=17 , color= "red", data = stations[stations$name==input$to_station_name,])
 		print(gg)
 	},height=600)
 
 
+#### fourth time analysis
+	output$heatmap <- renderPlot({
+		data_heat <- dataset()
+		#print(heatmap.2(table(data_heat[,c("from_month","from_hour")]),Rowv=FALSE,Cowv=FALSE))
+		heatmap.2(table(data_heat[,c("from_month","from_hour")]),Rowv=FALSE,Colv=FALSE,dendrogram="none",trace="none",scale="none")
 
+	},height=600)
 
 
 }
--- ui.R
+++ ui.R
@@ -15,7 +15,7 @@ fluidPage(
     selectInput('from_station_name', 'Start Station', c("ALL",unique(dataset$from_station_name))),
     selectInput('to_station_name', 'End Station', c("ALL",unique(dataset$to_station_name))),
 
-    sliderInput("binwidth", 'Binwidth (for Histogram)', min=10, max=200, value=50, step= 10),
+    sliderInput("binwidth_for_duration", 'Binwidth (for Duration Histogram)', min=10, max=200, value=50, step= 10),
 
     checkboxInput('by_age', 'By Age (for Histogram)'),
     checkboxInput('by_gender', 'By Gender (for Histogram)'),
@@ -33,7 +33,31 @@ fluidPage(
     	tabsetPanel(
 		tabPanel("Histogram",	
     			h3("Divvy Usage Duration Distribution"),
-    			plotOutput("plot")
+    			plotOutput("hist_for_duration"),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+    			h3("Divvy Usage Month Distribution"),
+    			plotOutput("hist_for_month"),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+    			h3("Divvy Usage Time Distribution"),
+    			plotOutput("hist_for_from_hour")
     		)
 		,
 		tabPanel("Stations Pairwise Analyses",
@@ -44,10 +68,37 @@ fluidPage(
 			plotOutput("plot_start")
 		)
 		,
-		tabPanel("End Stations Analyses",
+		tabPanel("Location Analyses",
+			h3("Distribution of the start station"),
+			textOutput("given_end"),
+			p("This map shows the distribution of the start station"),
+			p("The size of dots represent the total counts for each end station"),
+			plotOutput("ggmap_by_start"),
+
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			br(),
+			h3("Distribution of the end station"),
 			textOutput("given_start"),
-			p("This Map Shows The Distribution of the End Station"),
-			plotOutput("ggmap_by_start")
+			p("This map shows the distribution of the end station"),
+			p("The size of dots represent the total counts for each end station"),
+			plotOutput("ggmap_by_end")
+			
+		)
+		,
+		tabPanel("Month v.s. Time Analyses",
+			h3("Heatmap for Month and Time"),
+			textOutput("text_start1"),
+			textOutput("text_to1"),
+			plotOutput("heatmap")
 		)
  	)
   )
