ad016bf2e8abce679b0f3348d9ae5ed6d44bd2db
sppandlkk <u900222@gmail.com>
2015-07-11 19:49:44 -0500

    adding the load data part in

--- server.R
+++ server.R
@@ -1,7 +1,27 @@
 library(shiny)
 library(ggplot2)
 # read in the data
-#source("./load_data.r")
+trips <- read.csv("./data/Divvy_Trips_2013.csv")
+
+
+#### first restrict to member
+trips_subs <- trips[trips$usertype=="Subscriber",]
+trips_subs <- trips[!is.na(trips$birthday),]
+#### cut off extreme point
+trips_subs <- trips_subs[trips_subs$tripduration <= 1440,]
+
+####
+trips_subs$from_station_name <- as.character(trips_subs$from_station_name)
+trips_subs$to_station_name <- as.character(trips_subs$to_station_name)
+
+### create age variable
+trips_subs$age <- 2015 - trips_subs$birthday
+trips_subs$age_cat <- "0-30"
+trips_subs$age_cat[trips_subs$age >= 31] <- "31-35"
+trips_subs$age_cat[trips_subs$age >= 36] <- "36-40"
+trips_subs$age_cat[trips_subs$age >= 41] <- "41-34"
+trips_subs$age_cat[trips_subs$age >= 51] <- "51+"
+
 
 function(input, output) {
 		
@@ -31,6 +51,6 @@ function(input, output) {
 
     print(p)
 
-  }, height=700)
+  }, height=600)
 
 }
