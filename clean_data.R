ab504592c94f4ea0272b8eae0bec4ce223d297d2
sppandlkk <u900222@gmail.com>
2015-07-20 00:11:55 -0500

    new version of clean_data

--- clean_data.R
+++ clean_data.R
@@ -1,28 +1,48 @@
-#temp <- tempfile()
-#download.file("http://s3.amazonaws.com/divvy-data/datachallenge/datachallenge.zip",temp)
-#trips <- read.csv(unz(temp, "Data Challenge 2013_2014/Divvy_Stations_Trips_2013/Divvy_Trips_2013.csv"))
-#unlink(temp)
+
+clean_data <- function(data_in){
+	data_in$from_station_name <- as.character(data_in$from_station_name)
+	data_in$to_station_name <- as.character(data_in$to_station_name)
+
+	#### create age
+	data_in$age <- 2015 - data_in$birthday
+	data_in$age_cat <- "0-30"
+	data_in$age_cat[is.na(data_in$age)] <- NA
+	data_in$age_cat[data_in$age >= 31] <- "31-35"
+	data_in$age_cat[data_in$age >= 36] <- "36-40"
+	data_in$age_cat[data_in$age >= 41] <- "41-34"
+	data_in$age_cat[data_in$age >= 51] <- "51+"
+
+	data_in$from_month <- as.numeric(substr(data_in$starttime,6,7))
+	data_in$from_hour  <- as.numeric(substr(data_in$starttime,12,13))
+	data_in$to_hour    <- as.numeric(substr(data_in$stoptime,12,13))
+
+	data_in$from_time <- substr(data_in$starttime,12,16)
+	data_in$to_time   <- substr(data_in$stoptime,12,16)
 
 
-#stations <- read.csv("data/Divvy_Stations_2013.csv")
-#trips <- read.csv("data/Divvy_Trips_2013.csv")
+	data_in$start_date <- as.Date(substr(data_in$starttime,1,10),"%Y-%m-%d")
+	data_in$weekdays <- "weenday"
+	data_in$weekdays[weekdays(data_in$start_date) %in% c("Saturday","Sunday")] <- "weekend"
+
+	
+	return(data_in)
+}
 
 
-#### first restrict to member
-trips_subs <- trips[trips$usertype=="Subscriber",]
-trips_subs <- trips[!is.na(trips$birthday),]
-#### cut off extreme point
-trips_subs <- trips_subs[trips_subs$tripduration <= 1440,]
+clean_subs_data <- function(data_in){
+	data_in <- data_in[!is.na(data_in$birthday),]
+	#### cut off extreme point
+	data_in <- data_in[data_in$tripduration <= 3500,]
+	
+	#### cut off old people
+	data_in <- data_in[data_in$age <= 100,]
+	
+	
+}
 
-####
-trips_subs$from_station_name <- as.character(trips_subs$from_station_name)
-trips_subs$to_station_name <- as.character(trips_subs$to_station_name)
 
-### create age variable
-trips_subs$age <- 2015 - trips_subs$birthday
-trips_subs$age_cat <- "0-30"
-trips_subs$age_cat[trips_subs$age >= 31] <- "31-35"
-trips_subs$age_cat[trips_subs$age >= 36] <- "36-40"
-trips_subs$age_cat[trips_subs$age >= 41] <- "41-34"
-trips_subs$age_cat[trips_subs$age >= 51] <- "51+"
-#table(trips_subs$age_cat)
+
+
+
+
+
