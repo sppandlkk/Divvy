41a69222d6d6fb5968128afd8b933cf7028b36e2
sppandlkk <u900222@gmail.com>
2015-07-20 00:13:01 -0500

    move the cleaning part to clean_data. start to work on the trip suggestion

--- server.R
+++ server.R
@@ -4,40 +4,37 @@ library(ggmap)
 library(gplots)
 
 # read in the data
-trips <- read.csv("./data/Divvy_Trips_2013.csv")
+trips <- read.csv("./data/Divvy_Trips_2013_t.csv")
 stations <- read.csv("./data/Divvy_Stations_2013.csv")
+landmark <- read.csv("./data/landmark.csv")
+
+#### load function
+source("./clean_data.r")
+data_clean <- clean_data(trips)
 
 #### first restrict to member
-trips_subs <- trips[trips$usertype=="Subscriber",]
-trips_subs <- trips[!is.na(trips$birthday),]
-#### cut off extreme point
-trips_subs <- trips_subs[trips_subs$tripduration <= 2500,]
+trips_subs <- data_clean[data_clean$usertype=="Subscriber",]
+trips_cust <- data_clean[data_clean$usertype=="Customer",]
 
 ####
-trips_subs$from_station_name <- as.character(trips_subs$from_station_name)
-trips_subs$to_station_name <- as.character(trips_subs$to_station_name)
-
-### create age variable
-trips_subs$age <- 2015 - trips_subs$birthday
-trips_subs$age_cat <- "0-30"
-trips_subs$age_cat[trips_subs$age >= 31] <- "31-35"
-trips_subs$age_cat[trips_subs$age >= 36] <- "36-40"
-trips_subs$age_cat[trips_subs$age >= 41] <- "41-34"
-trips_subs$age_cat[trips_subs$age >= 51] <- "51+"
-
-#### cut off old people
-trips_subs <- trips_subs[trips_subs$age <= 90,]
+trips_subs <- clean_subs_data(trips_subs)
 
 
-#### create month
-#trips_subs$start_month <- format(as.Date(substr(trips_subs$starttime,1,7),"%m/%d/%y"),"%m")
-trips_subs$from_month <- as.numeric(substr(trips_subs$starttime,6,7))
-trips_subs$from_hour  <- as.numeric(substr(trips_subs$starttime,12,13))
-trips_subs$to_hour    <- as.numeric(substr(trips_subs$stoptime,12,13))
 
 function(input, output) {
 		
-  dataset <- reactive({
+  dataset_all <- reactive({
+	dd <- data_clean
+  	if(input$from_station_name != "ALL"){
+    		dd <- dd[dd$from_station_name %in% input$from_station_name,]
+    	}
+   	if(input$to_station_name != "ALL"){
+     		dd <- dd[dd$to_station_name %in% input$to_station_name,]
+     	}
+	return(dd)
+  })
+  
+    dataset_subs <- reactive({
 	dd <- trips_subs
   	if(input$from_station_name != "ALL"){
     		dd <- dd[dd$from_station_name %in% input$from_station_name,]
@@ -68,28 +65,27 @@ function(input, output) {
 
 
 #### first panel
+####
+	output$hist_for_weekdays <- renderPlot({
+		p <- ggplot(dataset_all(), aes(x=from_hour,fill=weekdays)) + geom_histogram(binwidth=1) + facet_grid(.~usertype) + xlim(0,24)
+		print(p)
+  }, height=300)
+  
+	output$hist_for_month <- renderPlot({
+		p <- ggplot(dataset_all(), aes(x=from_month,fill=weekdays)) + geom_histogram(binwidth=1) + facet_grid(.~usertype) + xlim(0,24)
+		print(p)
+  }, height=300)
+
+
+
 #### for creating the plot
   output$hist_for_duration <- renderPlot({
-    p <- qplot(tripduration,data=dataset(),  geom="histogram", binwidth=input$binwidth_for_duration)
-
-	facet_col <- "."	
-	facet_row <- "."
-
-    if (input$by_age)	facet_col <- "age_cat"
-    if (input$by_gender)	facet_row <- "gender"
-    
-  
-    facets <- paste(facet_row, '~', facet_col)
-    if (facets != '. ~ .')
-      p <- p + facet_grid(facets)
-
-
-    print(p)
-
-  }, height=600)
+		p <- ggplot(dataset_all(), aes(x=tripduration,fill=weekdays)) + geom_histogram(binwidth=input$binwidth_for_duration) + facet_grid(.~usertype) + xlim(0,input$limit_for_duration)
+		print(p)
+  }, height=300)
 
   output$hist_for_month <- renderPlot({
-    p <- qplot(from_month,data=dataset(),  geom="histogram", binwidth=1)
+    p <- qplot(from_month,data=dataset_all(),  geom="histogram", binwidth=1)
 
 	facet_col <- "."	
 	facet_row <- "."
@@ -106,8 +102,9 @@ function(input, output) {
     print(p)
 
   }, height=600)
+  
   output$hist_for_from_hour <- renderPlot({
-    p <- qplot(from_hour,data=dataset(),  geom="histogram", binwidth=1)
+    p <- qplot(from_hour,data=dataset_all(),  geom="histogram", binwidth=1)
 
 	facet_col <- "."	
 	facet_row <- "."
@@ -126,6 +123,54 @@ function(input, output) {
   }, height=600)
 
 #### second panel
+####  landmark 
+	output$landmark_table <- renderTable({
+		start_id <- stations[stations$name==input$trip_start_name,"id"]
+		chosen_station_id <- landmark[input$landmark_selected,"id"]
+		n_station <- length(chosen_station_id)
+		ss <- stations[stations$id %in% c(start_id,chosen_station_id),c("id","latitude", "longitude")]
+
+		best_route <- as.integer((solve_TSP(ETSP(ss[,2:3]))))
+		start_index <- which(best_route==1)
+		best_sol <- rep(best_route,2)[start_index:(start_index+n_station)]
+		
+		final_route <-do.call(rbind,lapply((c(start_id,chosen_station_id)[best_sol]),function(x) 					stations[stations$id==x,]))
+		final_route <- data.frame(rbind(final_route,final_route[1,]))
+### compute information
+### first create_new landmark
+		landmark$landmark <- as.character(landmark$landmark)
+		landmark_new <- rbind(landmark[,c("landmark","id")],c("Trip Start Station",start_id))
+		compute_avg_time <- do.call(rbind,
+		lapply(1:(n_station+1),function(i){
+			data_temp <- data_clean[(data_clean$from_station_id ==(final_route[i,"id"]))&(data_clean$to_station_id ==(final_route[i+1,"id"])),]
+			data.frame(
+			from=landmark_new[landmark_new$id==(final_route[i,"id"]),"landmark"],
+	  		to=landmark_new[landmark_new$id==(final_route[i+1,"id"]),"landmark"],
+
+#		  	to_station=stations[stations$id==final_route[i+1,"id"],"name"],
+#			from_station=stations[stations	$id==final_route[i,"id"],"name"],			
+	  		number_of_route=dim(data_temp)[1],avg_time_in_sec=mean(data_temp$tripduration))
+		}))
+		compute_avg_time
+
+	})
+	
+	output$landmark_crosswalk <- renderTable({
+		start_id <- stations[stations$name==input$trip_start_name,"id"]
+		chosen_station_id <- landmark[input$landmark_selected,"id"]
+
+		dd <- rbind(
+		cbind(landmark="Trip Start Station",stations[stations$name==input$trip_start_name,c("name","latitude","longitude","dpcapacity")])
+		,
+		landmark[landmark$id %in% chosen_station_id,c("landmark","name","latitude","longitude","dpcapacity")]
+		)		
+		names(dd)[2] <- "Divvy Station Name"		
+		row.names(dd) <- NULL
+		dd
+		
+	})
+
+
 #### start station analysis
   output$text_start <- renderText({
 	 paste("You have selected start station :", input$from_station_name)
@@ -142,7 +187,7 @@ function(input, output) {
 
 #### plot 
   output$plot_start <- renderPlot({
-	p_start <- qplot(age,tripduration, data=dataset(),color=gender,alpha=I(1/input$alpha))
+	p_start <- qplot(age,tripduration, data=dataset_all(),color=gender,alpha=I(1/input$alpha))
 	print(p_start)
   })
 
@@ -190,7 +235,7 @@ function(input, output) {
 
 #### fourth time analysis
 	output$heatmap <- renderPlot({
-		data_heat <- dataset()
+		data_heat <- dataset_all()
 		#print(heatmap.2(table(data_heat[,c("from_month","from_hour")]),Rowv=FALSE,Cowv=FALSE))
 		heatmap.2(table(data_heat[,c("from_month","from_hour")]),Rowv=FALSE,Colv=FALSE,dendrogram="none",trace="none",scale="none")
 
